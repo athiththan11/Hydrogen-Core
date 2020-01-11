97c9bab47678ca6b3ccf597d3da3ddbf6f583b51
Athiththan <code19111995@gmail.com>
2020-01-11 23:25:11 +0530

    multiple gateway deployment commit

--- src/executions/deployment/apim/deployment.multiplegw.js
+++ src/executions/deployment/apim/deployment.multiplegw.js
@@ -1,2 +1,55 @@
 'use strict';
 
+const {
+	alterAuthManagerServerURL,
+	alterAPIKeyValidatorServerURL,
+	alterAPIKeyValidatorThriftClientPort,
+	alterAPIKeyValidatorEnableThriftServer,
+	alterOAuthConfigurationRevokeAPIURL,
+	addGatewayEnvironment,
+} = require('../../../modules/api.manager/apimanager.util');
+const { configurePortOffset } = require('../../../modules/carbon/carbon.util');
+
+const { logger } = require('../../../utils/util.winston');
+
+/**
+ * method to configure apim gateway pack for multiple-gateway layout
+ *
+ * @param {*} workingDir path of the working directory
+ * @param {*} layoutConfs layout configuration arguments
+ */
+async function configureGateway(
+	workingDir,
+	layoutConfs = { _hostname: 'https://localhost', thriftClientPort: '10397', enableThriftServer: 'false', offset: 1 }
+) {
+	if (process.env.HYDROGEN_DEBUG) logger.debug('Configuring API Manager as Gateway');
+
+	await alterAuthManagerServerURL(layoutConfs, workingDir);
+	await alterAPIKeyValidatorServerURL(layoutConfs, workingDir);
+	await alterAPIKeyValidatorThriftClientPort(layoutConfs, workingDir);
+	await alterAPIKeyValidatorEnableThriftServer(layoutConfs, workingDir);
+	await alterOAuthConfigurationRevokeAPIURL(layoutConfs, workingDir);
+
+	await configurePortOffset(workingDir, layoutConfs.offset);
+}
+
+async function configureGatewayAIO(workingDir, environmentConfsArray = []) {
+	if (process.env.HYDROGEN_DEBUG) logger.debug('Configuring API Manager for Multiple Gateway Setup');
+
+	// TODO: configurations
+	// remove default : optional
+	// add many gateway environments
+
+	// loop through environmentConfsArray and configure AIO pack
+	// TESTME: check the loop function
+	environmentConfsArray.forEach((envConfs) => {
+		let index = environmentConfsArray.indexOf(envConfs);
+		((index) => {
+			if (process.env.HYDROGEN_DEBUG) logger.debug('Add environment ' + index);
+			addGatewayEnvironment(envConfs, workingDir);
+		})(index);
+	});
+}
+
+exports.configureGateway = configureGateway;
+exports.configureGatewayAIO = configureGatewayAIO;
