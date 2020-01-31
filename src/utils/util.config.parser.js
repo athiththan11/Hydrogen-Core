931a579574e4b4a92d53e00548b39afec0adeb87
athiththan11 <code19111995@gmail.com>
2020-02-01 00:02:23 +0530

    :sparkles: config parser and validator

---
+++ src/utils/util.config.parser.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+const Ajv = require('ajv');
+
+const { logger } = require('./util.winston');
+
+/**
+ * method to read and parse confgurations
+ *
+ * @param {string} configPath config file path
+ * @returns {{}} parsed configuration string
+ */
+async function configParser(configPath) {
+	try {
+		let parsed = fs.readFileSync(configPath, 'utf8');
+		parsed = JSON.parse(parsed);
+		return parsed;
+	} catch (err) {
+		logger.error(err);
+		return null;
+	}
+}
+
+/**
+ * method to validate the JSON schema and configurations
+ *
+ * @param {{}} schema schema
+ * @param {{}} confs configurations
+ * @returns {{valid: boolean, message: string}}
+ */
+async function validateConfigs(schema, confs) {
+	let ajv = new Ajv();
+	let valid = false,
+		errorMessage = null;
+	try {
+		valid = ajv.validate(schema, confs);
+	} catch (err) {
+		errorMessage = (ajv.errors || [])
+			.map((error) => {
+				try {
+					const [, index, fieldName] = /\[(.*)\].(.*)/.exec(error.dataPath);
+					return `error with item #${index}'s field "${fieldName}". The error is: ${error.message}`;
+				} catch (error) {
+					return error.message;
+				}
+			})
+			.join('\n');
+	}
+	return { valid, errorMessage };
+}
+
+exports.configParser = configParser;
+exports.validateConfigs = validateConfigs;
