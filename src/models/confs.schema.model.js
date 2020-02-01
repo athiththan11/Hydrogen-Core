2f3a832cf7742267e77beadf2f774c3b322ea31d
athiththan11 <code19111995@gmail.com>
2020-02-01 23:21:24 +0530

    schema updated is-km

--- src/models/confs.schema.model.js
+++ src/models/confs.schema.model.js
@@ -55,6 +55,7 @@ let publishMultipleGatewaySchema = {
 					minimum: 1,
 				},
 			},
+			required: ['_hostname', 'thriftClientPort', 'enableThriftServer', 'offset'],
 		},
 	},
 };
@@ -113,8 +114,84 @@ let layoutConfsSchema = {
 			minimum: 1,
 		},
 	},
+	required: ['_hostname', 'thriftClientPort', 'enableThriftServer', 'offset'],
+};
+
+let iskmSchema = {
+	$id: 'iskm.schema',
+	type: 'object',
+	properties: {
+		apimlayoutConfs: {
+			type: 'object',
+			properties: {
+				_hostname: {
+					type: 'string',
+				},
+				keyValidatorClientType: {
+					type: 'string',
+				},
+				enableThriftServer: {
+					type: 'string',
+				},
+				iskmoffset: {
+					type: 'number',
+				},
+			},
+			required: ['_hostname', 'keyValidatorClientType', 'enableThriftServer', 'iskmoffset'],
+		},
+		iskmlayoutConfs: {
+			type: 'object',
+			properties: {
+				_hostname: {
+					type: 'string',
+				},
+				offset: {
+					type: 'number',
+				},
+			},
+			required: ['_hostname', 'offset'],
+		},
+	},
+};
+
+let apimlayoutConfs = {
+	$id: 'apim.confs.schema',
+	type: 'object',
+	properties: {
+		_hostname: {
+			type: 'string',
+		},
+		keyValidatorClientType: {
+			type: 'string',
+		},
+		enableThriftServer: {
+			type: 'string',
+		},
+		iskmoffset: {
+			type: 'number',
+		},
+	},
+	required: ['_hostname', 'keyValidatorClientType', 'enableThriftServer', 'iskmoffset'],
+};
+
+let iskmlayoutConfs = {
+	$id: 'iskm.confs.schema',
+	type: 'object',
+	properties: {
+		_hostname: {
+			type: 'string',
+		},
+		offset: {
+			type: 'number',
+		},
+	},
+	required: ['_hostname', 'offset'],
 };
 
 exports.publishMultipleGatewaySchema = publishMultipleGatewaySchema;
 exports.environmentConfsSchema = environmentConfsSchema;
 exports.layoutConfsSchema = layoutConfsSchema;
+
+exports.iskmSchema = iskmSchema;
+exports.apimlayoutConfs = apimlayoutConfs;
+exports.iskmlayoutConfs = iskmlayoutConfs;
