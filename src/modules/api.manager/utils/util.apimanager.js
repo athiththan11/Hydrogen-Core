bb3d2aa6c19416a9566a232a2fde084986c51259
athiththan11 <code19111995@gmail.com>
2020-05-24 15:26:10 +0530

    api manager util

--- src/modules/api.manager/utils/util.apimanager.js
+++ src/modules/api.manager/utils/util.apimanager.js
@@ -44,4 +44,29 @@ function constructGatewayEnvironment(Element, xmlDoc, args) {
 	return environmentElem;
 }
 
+/**
+ * method to construct gateway environment configurations for toml
+ *
+ * @param {{}} args environment parameters and connection strings
+ * @returns {{}} constructed envrionment element
+ */
+function constructGatewayEnvironmentToml(args) {
+	let obj = {
+		name: args._name,
+		type: args.type,
+		display_in_api_console: args['api-console'],
+		description: args._description,
+		show_as_token_endpoint_url: true,
+		service_url: 'https://' + args._hostname + ':' + (ports._9443 + args.offset) + '/services/',
+		username: args.username ? args.username : '${admin.username}',
+		password: args.password ? args.password : '${admin.password}',
+		ws_endpoint: 'ws://' + args._hostname + ':' + (ports._9099 + args.offset),
+		wss_endpoint: 'wss://' + args._hostname + ':' + (ports._8099 + args.offset),
+		http_endpoint: 'http://' + args._hostname + ':' + (ports._8280 + args.offset),
+		https_endpoint: 'https://' + args._hostname + ':' + (ports._8243 + args.offset),
+	};
+	return obj;
+}
+
 exports.constructGatewayEnvironment = constructGatewayEnvironment;
+exports.constructGatewayEnvironmentToml = constructGatewayEnvironmentToml;
