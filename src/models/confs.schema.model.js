fb7ff3f7819fd1f24cbc9a251d39d2200b57799b
athiththan11 <code19111995@gmail.com>
2020-06-14 00:28:23 +0530

    :pencil2: typo fix

--- src/models/confs.schema.model.js
+++ src/models/confs.schema.model.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let publishMultipleGatewaySchema = {
-	$id: 'publismultiplegateway.schema',
+	$id: 'publishmultiplegateway.schema',
 	type: 'object',
 	properties: {
 		environmentConfs: {
